241730a20cdecd38ce4f04b44a31f2bb0611b8cd
joscha-sat <joscha.sattler@web.de>
2021-05-25 17:02:50 +0200

    check18

--- src/app/components/kunden/kunden.component.ts
+++ src/app/components/kunden/kunden.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Kunde } from 'src/app/interfaces/kunde';
 import { KundeHttpService } from 'src/app/services/kunde-http.service';
 
@@ -12,13 +11,36 @@ export class KundenComponent implements OnInit {
   // --------------------------------------------------------------------------------- || Constructor ||
   constructor(private http: KundeHttpService) {}
 
-  // ------------------------------------------------------------------------- || Variables + Objects ||
+  // --------------------------------------------------------------------------------- || Variables + Objects ||
 
-  //  mit Keyword | async in HTML
+  users: Kunde[];
 
-  asyncUsers: Observable<Kunde[]> = this.http.getUsers();
+  ueber18(): void {
+    const users: Kunde[] = this.users;
+    const today: Date = new Date();
+    const vor18Jahren: Date = new Date(
+      today.getFullYear() - 18,
+      today.getMonth(),
+      today.getDate()
+    );
+
+    let i: number;
+
+    for (i = 0; i < users.length; i++) {
+      const geburtsdatum = new Date(users[i].geburtsdatum);
+      console.log(geburtsdatum < vor18Jahren);
+    }
+  }
+
+  // ------------------------------------------------------------------------------------ || methods ||
+
+  getAllUsers(): void {
+    this.http.getUsers().subscribe((user) => (this.users = user));
+  }
 
   // ------------------------------------------------------------------------------------ || ngOnInit ||
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
 }
